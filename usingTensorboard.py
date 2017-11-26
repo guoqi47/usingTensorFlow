04dfb2a3a4c0851eb313df5205fadfd095db8cc5
guoqi47 <guoqi47@163.com>
2017-11-26 22:39:28 +0800

    2017-11-26

--- usingTensorboard.py
+++ usingTensorboard.py
@@ -2,25 +2,28 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-def add_layer(inputs,in_size,out_size,activation_function=None): #先不定义激励函数
-    #初始权重为随机变量要比全零要好;矩阵一般开头大写，后面的向量biase一般小写
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):  # 先不定义激励函数
+    # 初始权重为随机变量要比全零要好;矩阵一般开头大写，后面的向量biase一般小写
     with tf.name_scope('layer'):
         with tf.name_scope('weights'):
-            Weight = tf.Variable(tf.random_normal([in_size,out_size]))
+            Weight = tf.Variable(tf.random_normal([in_size, out_size]))
         with tf.name_scope('biases'):
-            biases = tf.Variable(tf.zeros([1,out_size])+0.1)
-        #未激活的预测值
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+        # 未激活的预测值
         with tf.name_scope('Wx_plus_b'):
-            Wx_plus_b = tf.matmul(inputs,Weight)+biases
+            Wx_plus_b = tf.matmul(inputs, Weight) + biases
         if activation_function is None:
-            outputs=Wx_plus_b
+            outputs = Wx_plus_b
         else:
-            outputs=activation_function(Wx_plus_b)
+            outputs = activation_function(Wx_plus_b)
         return outputs
+
 
 # linspace(x1,x2,N) 用于产生x1,x2之间的N点行线性的矢量。
 # 其中x1、x2、N分别为起始值、终止值、元素个数
-x_data = np.linspace(-1,1,300)[:,np.newaxis]
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 # numpy.random.normal(loc=0.0, scale=1.0, size=None),均值，标准差，形状
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
@@ -32,11 +35,12 @@ with tf.name_scope('inputs'):
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 prediction = add_layer(l1, 10, 1, activation_function=None)
 with tf.name_scope('loss'):
-    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
-writer = tf.train.SummeryWriter('logs/',sess.graph)
+# 把整个框架加载到文件里面去，graph是整个的框架，sess.graph是收集整个框架放在文件里
+writer = tf.summary.FileWriter('logs/', sess.graph)
 sess.run(init)
